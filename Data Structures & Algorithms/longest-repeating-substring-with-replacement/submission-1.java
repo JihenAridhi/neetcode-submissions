069c833a43aa7cc61869139339d2d4606780df9b
Jihen Aridhi <114924283+JihenAridhi@users.noreply.github.com>
2026-09-15 15:32:45 +0100

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int maxF = 0;
+        int res = 0;
+        Map<Character, Integer> frequency = new HashMap<>();
+
+        int start = 0, end = 0;
+        while (start <= end && end < s.length()) {
+            frequency.put(s.charAt(end), frequency.getOrDefault(s.charAt(end), 0) + 1);
+            System.out.println(s.charAt(end) + ":" + frequency.get(s.charAt(end)));
+            maxF = Math.max(maxF, frequency.get(s.charAt(end)));
+            int window = end - start + 1;
+            int gap = window - maxF;
+            if (gap > k) {
+                frequency.put(s.charAt(start), frequency.get(s.charAt(start)) - 1);
+                start++;
+            } else
+                res = window;
+            end++;
+        }
+
+        return res;
+    }
+}
